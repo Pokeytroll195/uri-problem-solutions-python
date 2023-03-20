7c6edaa24c8e1a53cd0f7b91a798aad155b901f1
Nazran Sami <33898840+Pokeytroll195@users.noreply.github.com>
2023-03-20 13:33:54 +0600

    Create 1115 - Quadrant.py

---
+++ 1115 - Quadrant.py
@@ -0,0 +1,13 @@
+flag = True
+while flag:
+    x, y = map(int, input().split(' '))
+    if x == 0 or y == 0:
+        flag = False
+    elif x > 0 and y > 0:
+        print('primeiro')
+    elif x > 0 > y:
+        print('quarto')
+    elif x < 0 < y:
+        print('segundo')
+    elif x < 0 and y < 0:
+        print('terceiro')
